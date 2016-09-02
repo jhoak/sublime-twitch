1e33c1bf8a34404bf2013538af3603b769899a50
James Hoak <jah272@case.edu>
2016-09-02 00:58:54 -0400

    Make file readable + add ideas

--- startup.py
+++ startup.py
@@ -1,37 +1,60 @@
+# Ideas list
+# - be able to change streams at any time w/ hotkey/button/whatever
+# - toggle webcam (prompt though)
+# - handle stream ending / re-prompt
+# - handle internet ending / re-prompt
+# - toggle chat
+# - chat fcn'ality
+# - chat/video size
+# - reposition minimap?
+# - handle text getting under chat sidebar
+# - decide on sidebar vs overlay (if that's even a choice)
+
 from tkinter import *
+
+def connect(streamer_name, login_name, passwd):
+	"""Connects user to a Twitch stream."""
+	#if stream:
+		# ... Try to get the stream view/chat, show if ok(?) and return
+		# ... also incorporate login
+	# ... if we messed up then show a dialog saying we couldn't connect.
+	pass
+
+# Main window initialization
 basewindow = Tk()
 basewindow.title("Twitch Plugin Options")
 basewindow.geometry('360x200')
 basewindow.resizable(width=False, height=False)
-topf, namef, passf, checkf, botf = (Frame(basewindow) for i in range(5))
-# Build top
+
+# Now for frames
+topf, checkf, botf = (Frame(basewindow) for i in range(3))
+
+# Build top frame (stream name, username, password (latter 2 optional))
 Label(topf, text='Name of streamer to watch (required):', anchor=E).grid(row=0, column=0, pady=10)
 Label(topf, text='Login name (optional):', anchor=E).grid(row=1, column=0, pady=10)
 Label(topf, text='Password (if logging in):', anchor=E).grid(row=2, column=0, pady=10)
-stream_name_field = Entry(topf)
+stream_name_field, username_field, passwd_field = (Entry(topf) if i != 2 else Entry(topf, show='*') for i in range(3))
 stream_name_field.grid(row=0, column=1)
-username_field = Entry(topf)
 username_field.grid(row=1, column=1)
-passwd_field = Entry(topf)
 passwd_field.grid(row=2, column=1)
 topf.pack(side=TOP)
-# Build name, password frames
-#username_field = make_textframe(namef, 'Login name (optional): ')
-#passwd_field = make_textframe(passf, 'Password (only if logging in): ')
+
 # Build checkbox frame
 webcam_check, box_text = IntVar(), '(If streaming) Show webcam view'
 Checkbutton(checkf, text=box_text, variable=webcam_check).pack()
 checkf.pack(pady=10)
+
 # Build bottom
 def try_ok():
-	stream = stream_name_field.get()
-	#if stream:
-		# ... Try to get the stream view/chat, show if ok(?) and return
-	# ... if we messed up then show a dialog saying we couldn't connect.
+	"""Tries to connect w/ given info (passes it to connect())"""
+	pass
+	
 botf_left, botf_right = (Frame(botf) for i in range(2))
 Button(botf_left, text='OK', width=10, command=try_ok).pack()
 Button(botf_right, text='Cancel', width=10, command=quit).pack()
 botf_left.grid(row=0, column=0, pady=0, padx=10)
 botf_right.grid(row=0, column=1, pady=0, padx=10)
 botf.pack()
+
+# Run!
 basewindow.mainloop()
